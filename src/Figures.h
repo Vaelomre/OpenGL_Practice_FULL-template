00dfbe74c6567ba81a570df00c5eb71630a01070
user <user@Mac-mini-4.local>
2018-04-19 12:04:35 +0300

    fix textures

--- src/Figures.h
+++ src/Figures.h
@@ -10,9 +10,9 @@ using namespace glm;
 int triangleVertexCount = 3;
 Vertex triangleVertexes[] = {
     //                ВЕРШИНА                   ЦВЕТ
-    Vertex(vec3( 0.0f,  1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f), vec2(1.f, 1.f)),
-    Vertex(vec3(-1.0f, -1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f), vec2(0.5f, 0.5f)),
-    Vertex(vec3( 1.0f, -1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f))
+    Vertex(vec3( 0.0f,  1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f), vec2( 0.0f,  1.0f)),
+    Vertex(vec3(-1.0f, -1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f), vec2(-1.0f, -1.0f)),
+    Vertex(vec3( 1.0f, -1.0f,  0.0f),   vec3(0.5f, 0.8f, 0.5f), vec2( 1.0f, -1.0f))
 };
 
 #endif
